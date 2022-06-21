31febc60f8a0d53240f5c0e39cd61c561343e9fd
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2022-06-21 12:32:32 +0200

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import minimist from "minimist";
 // NOTE: differentiate between positional arguments, and options passed after "--" POSIX separator
 const argv = minimist(process.argv.slice(2), { "--": true, string: ["--", "_"] });
 await (async () => {
-    let version = argv.v || argv.version;
+    const version = argv.v || argv.version;
     if (version || argv.h || argv.help) {
         (await import("./cli/index-cli.js")).default(import.meta.url, version);
         process.exit(0);
